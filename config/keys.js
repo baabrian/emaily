a28b8231ca47311e34e90b0c7d2f014b208d88f1
Brian Nguyen <brianlnguyen12@gmail.com>
2020-08-23 23:01:51 -0700

    check prod and dev

---
+++ config/keys.js
@@ -0,0 +1,10 @@
+//keys.js - figure out what set of credentials to return
+
+if(process.env.NODE_ENV === 'production') {
+  //return prod set of keys
+  module.exports = require('./prod');
+} else {
+  //return set of dev keys
+  module.exports = require('./dev');
+}
+
