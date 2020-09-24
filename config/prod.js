8e4034cfe4dd4106ef18bbbb2b8c0f0f499491c7
Brian Nguyen <brianlnguyen12@gmail.com>
2020-09-24 02:01:36 -0700

    updated prod.js and removed dev key

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,5 @@
 //prod.js - production keys here!!!!!!!!
 
-const { stripePublishableKey } = require("./dev");
-
 module.exports = {
   googleClientId: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
