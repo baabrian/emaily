996d98b418a7017cca22e9869c4330b9f2380e61
Brian Nguyen <brianlnguyen12@gmail.com>
2020-10-05 22:33:54 -0700

    added new action type

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { FETCH_USER } from "./types";
+import { FETCH_USER, FETCH_SURVEYS } from "./types";
 
 export const fetchUser = () => async (dispatch) => {
   const res = await axios.get("/api/current_user");
@@ -19,8 +19,14 @@ export const handleToken = (token) => async (dispatch) => {
   });
 };
 
-export const submitSurvey = (values,history) => async (dispatch) => {
+export const submitSurvey = (values, history) => async (dispatch) => {
   const res = axios.post("/api/survey", values);
-  history.push('/surveys');
+  history.push("/surveys");
   dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+export const fetchSurveys = () => async (dispatch) => {
+  const res = await axios.get("/api/surveys");
+  console.log(res.data);
+  dispatch({ type: FETCH_SURVEYS, payload: res.data });
 };
